86ea4278ce999480189eaba174a19c2bf1ec4287
Andre Costa <andre2396costa@gmail.com>
2025-01-09 22:25:51 -0300

    Views and controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Enums\SignatureStatus;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SignatureController;
+use App\Models\Plan;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,3 +22,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/teste', [SignatureController::class, 'index']);
